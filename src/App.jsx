d9a15e567bb0b9493105d8e01c5bfeb4925519aa
Dongjoo Lee <dj980907@gmail.com>
2024-05-01 19:13:22 -0400

    roadmap done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
-// import Roadmap from "./components/Roadmap";
+import Roadmap from "./components/Roadmap";
 // import Services from "./components/Services";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Collaboration />
         {/* <Services /> */}
         <Pricing />
-        {/* <Roadmap /> */}
+        <Roadmap />
         {/* <Footer /> */}
       </div>
 
