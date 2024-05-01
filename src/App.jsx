16c46907fae1039f22d6c96347df8955cb89080e
Dongjoo Lee <dj980907@gmail.com>
2024-05-01 18:53:24 -0400

    benefits section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Benefits from "./components/Benefits";
+// import Collaboration from "./components/Collaboration";
+// import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+// import Pricing from "./components/Pricing";
+// import Roadmap from "./components/Roadmap";
+// import Services from "./components/Services";
 
 function App() {
   return (
@@ -9,12 +14,12 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        {/* <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
-        <Footer /> */}
+        <Benefits />
+        {/* <Collaboration /> */}
+        {/* <Services /> */}
+        {/* <Pricing /> */}
+        {/* <Roadmap /> */}
+        {/* <Footer /> */}
       </div>
 
       <ButtonGradient />
