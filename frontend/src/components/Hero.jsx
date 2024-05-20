6f34970736df4ec01c26f82f4098163a98f6f1a4
Dongjoo Lee <dj980907@gmail.com>
2024-05-20 17:49:27 -0400

    added padding between hero and next section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -104,7 +104,7 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
 
-        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block pb-20" />
       </div>
 
       <BottomLine />
